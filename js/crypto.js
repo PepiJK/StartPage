b1bbc6f140fc810d663cfb4fb70ba2c0c3657ee8
PepiJK <josef.koch97@gmail.com>
2018-08-14 11:09:06 +0200

    new icon, new link, js changes

--- js/crypto.js
+++ js/crypto.js
@@ -1,15 +1,15 @@
-var btc = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR'
-var xmlhttp2 = new XMLHttpRequest()
+var btc = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=EUR';
+var xmlhttp2 = new XMLHttpRequest();
 
 xmlhttp2.onreadystatechange = function () {
   if (this.readyState === 4 && this.status === 200) {
-    var b = JSON.parse(this.responseText)
-    output2(b)
+    var b = JSON.parse(this.responseText);
+    output2(b);
   }
 }
-xmlhttp2.open('GET', btc, true)
-xmlhttp2.send()
+xmlhttp2.open('GET', btc, true);
+xmlhttp2.send();
 
 function output2 (data) {
-  document.getElementById('btc').innerHTML = '<a href="https://coinmarketcap.com/"><img src="img/btc.png">' + 'BTC: ' + data.EUR.toFixed(2) + '€</a>'
+  document.getElementById('btc').innerHTML = '<a href="https://coinmarketcap.com/"><img src="img/btc.png">' + 'BTC: ' + data.EUR.toFixed(2) + '€</a>';
 }
--- js/timedate.js
+++ js/timedate.js
@@ -1,12 +1,12 @@
 function timedate () {
-  var d = new Date()
-  var days = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag']
-  var months = ['Jänner', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
-  document.getElementById('time').innerHTML = ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2)
-  document.getElementById('date').innerHTML = days[d.getDay()] + ', ' + d.getDate() + '. ' + months[d.getMonth()] + ' ' + d.getFullYear()
+  var d = new Date();
+  var days = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+  var months = ['Jänner', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
+  document.getElementById('time').innerHTML = ('0' + d.getHours()).slice(-2) + ':' + ('0' + d.getMinutes()).slice(-2);
+  document.getElementById('date').innerHTML = days[d.getDay()] + ', ' + d.getDate() + '. ' + months[d.getMonth()] + ' ' + d.getFullYear();
 }
 
 window.onload = function () {
-  timedate()
-  setInterval(timedate, 1000)
+  timedate();
+  setInterval(timedate, 1000);
 }
